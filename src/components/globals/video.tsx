a00f8f22e20332126dc814d753a4ce864ccd5bf9
Daniel Campos <daniel.campos@devio.com.br>
2024-05-19 16:17:28 -0300

    fix(mp4 to gif)

--- src/components/globals/video.tsx
+++ src/components/globals/video.tsx
@@ -1,30 +1,28 @@
 'use client';
 import useVideo from '@/hooks/useVideo';
+import Image, { StaticImageData } from 'next/image';
 
-export default function VideoLogin({
-  Video,
-  loop,
-}: {
-  Video: string;
-  loop?: boolean;
-}) {
+export default function VideoLogin({ Video }: { Video: StaticImageData }) {
   const { handleVideoEnd, videoExist, videoFadeOut, videoRef } = useVideo();
   if (!videoExist) {
     return null;
   }
   return (
-    <video
-      ref={videoRef}
+    <Image
+      alt="loading gif"
       src={Video}
-      autoPlay
-      muted
-      loop={loop}
-      onClick={!loop ? handleVideoEnd : undefined}
-      onEnded={handleVideoEnd}
-      className="w-full object-cover absolute top-0 left-0 z-10 bg-black/50"
+      unoptimized
+      ref={videoRef}
+      onClick={handleVideoEnd}
       style={{
         transition: 'opacity 1s cubic-bezier(0.85, 0, 0.15, 1)',
         opacity: videoFadeOut ? 0 : 1,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        objectFit: 'cover',
+        height: '100%',
+        zIndex: 11,
       }}
     />
   );
