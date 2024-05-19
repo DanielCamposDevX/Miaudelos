499039684c8a69f6a991e06891ecf906021ed81e
Daniel Campos <daniel.campos@devio.com.br>
2024-05-19 16:01:23 -0300

    fix(login & signup): fixed height problem

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,43 +10,45 @@ import Image from 'next/image';
 export default function Login() {
   return (
     <DefaultProviders>
-      <Image
-        src={CatsBG}
-        alt="Cats Background"
-        objectFit="cover"
-        style={{
-          position: 'absolute',
-          zIndex: '-1',
-          top: '0',
-          left: '0',
-          height: '100%',
-          width: '100%',
-          objectFit: 'cover',
-        }}
-      />
-      <div className="w-full h-full flex flex-col items-center justify-around bg-black/10 z-0">
-        <div
-          className={`border rounded-2xl flex flex-col h-4/6 w-11/12 max-w-[400px] items-center justify-center gap-14 relative overflow-hidden z-10 border-black shadow-lg`}
-        >
-          <Image
-            src={CatsBOXBG}
-            alt="Cats Background"
-            style={{
-              position: 'absolute',
-              zIndex: '-1',
-              top: '0',
-              left: '0',
-              height: '100%',
-              width: '100%',
-              opacity: '1',
-              objectFit: 'cover',
-            }}
-          />
-          <div className="w-10/12 border rounded-3xl bg-white flex justify-center items-center border-black shadow-lg">
-            <Image src={Logo} alt={'Miaudelos'} className="w-11/12" />
+      <div className="w-full h-screen relative overflow-hidden">
+        <Image
+          src={CatsBG}
+          alt="Cats Background"
+          objectFit="cover"
+          style={{
+            position: 'absolute',
+            zIndex: '-1',
+            top: '0',
+            left: '0',
+            height: '100%',
+            width: '100%',
+            objectFit: 'cover',
+          }}
+        />
+        <div className="w-full h-full flex flex-col items-center justify-around bg-black/10 z-0">
+          <div
+            className={`border rounded-2xl flex flex-col h-4/6 w-11/12 max-w-[400px] items-center justify-center gap-14 relative overflow-hidden z-10 border-black shadow-lg`}
+          >
+            <Image
+              src={CatsBOXBG}
+              alt="Cats Background"
+              style={{
+                position: 'absolute',
+                zIndex: '-1',
+                top: '0',
+                left: '0',
+                height: '100%',
+                width: '100%',
+                opacity: '1',
+                objectFit: 'cover',
+              }}
+            />
+            <div className="w-10/12 border rounded-3xl bg-white flex justify-center items-center border-black shadow-lg">
+              <Image src={Logo} alt={'Miaudelos'} className="w-11/12" />
+            </div>
+            <LoginForm />
+            <VideoLogin Video={EntranceVideo} />
           </div>
-          <LoginForm />
-          <VideoLogin Video={EntranceVideo} />
         </div>
       </div>
     </DefaultProviders>
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -8,45 +8,47 @@ import Image from 'next/image';
 export default function Singup() {
   return (
     <DefaultProviders>
-      <Image
-        src={CatsBG}
-        alt="Cats Background"
-        objectFit="cover"
-        style={{
-          position: 'absolute',
-          zIndex: '-1',
-          top: '0',
-          left: '0',
-          height: '100%',
-          width: '100%',
-          objectFit: 'cover',
-        }}
-      />
-      <div className="w-full h-full flex flex-col items-center justify-around bg-black/10 z-0">
-        <div
-          className={`border rounded-2xl py-10 flex flex-col w-11/12 max-w-[400px] items-center justify-center relative z-10 border-black shadow-lg bg-[#E4E3E0] overflow-hidden`}
-        >
-          <Image
-            src={CreateBG}
-            alt="Cats Background"
-            objectFit="cover"
-            style={{
-              position: 'absolute',
-              zIndex: '-1',
-              top: '0',
-              left: '0',
-              height: '100%',
-              width: '100%',
-              objectFit: 'cover',
-              opacity: '0.4',
-            }}
-          />
-          <Image
-            src={CatsRegister}
-            alt="Cats Register"
-            style={{ objectFit: 'cover', height: '200px', width: '200px' }}
-          />
-          <SignupForm />
+      <div className="w-full h-screen relative overflow-hidden">
+        <Image
+          src={CatsBG}
+          alt="Cats Background"
+          objectFit="cover"
+          style={{
+            position: 'absolute',
+            zIndex: '-1',
+            top: '0',
+            left: '0',
+            height: '100%',
+            width: '100%',
+            objectFit: 'cover',
+          }}
+        />
+        <div className="w-full h-full flex flex-col items-center justify-around bg-black/10 z-0">
+          <div
+            className={`border rounded-2xl py-10 flex flex-col w-11/12 max-w-[400px] items-center justify-center relative z-10 border-black shadow-lg bg-[#E4E3E0] overflow-hidden`}
+          >
+            <Image
+              src={CreateBG}
+              alt="Cats Background"
+              objectFit="cover"
+              style={{
+                position: 'absolute',
+                zIndex: '-1',
+                top: '0',
+                left: '0',
+                height: '100%',
+                width: '100%',
+                objectFit: 'cover',
+                opacity: '0.4',
+              }}
+            />
+            <Image
+              src={CatsRegister}
+              alt="Cats Register"
+              style={{ objectFit: 'cover', height: '200px', width: '200px' }}
+            />
+            <SignupForm />
+          </div>
         </div>
       </div>
     </DefaultProviders>
